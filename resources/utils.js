5a4f7107d7f8cf07f58edb4ac4634683619a088b
Felippe <felippescheidt@gmail.com>
2023-03-26 14:15:27 -0300

    update: 10-align.html 20-justify.html 30-direction

--- resources/utils.js
+++ resources/utils.js
@@ -7,10 +7,18 @@ function html2text(targetEl) {
   let target = targetEl || ".snippet";
   document.querySelectorAll(target).forEach(function(el) {
     let codeEl = el.querySelector("code.sample");
-    let sampleResultEl = el.querySelector(".sample-result");
+    let sampleResultEl = el.querySelector(".sample-render");
     sampleResultEl.innerHTML = codeEl.innerHTML;
   });
   document.querySelectorAll("code").forEach(function(element) {
   element.innerHTML = element.innerHTML.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
   });
+}
+function getStyle(){
+  let style = document.querySelector('style').innerHTML;
+  style = style.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  document.querySelector('#source').innerHTML = style;
+  document.querySelector('#codeWrapper').style.display="block";
+  document.querySelector('#codeWrapper').scrollIntoView();
+  hljs.highlightAll();
 }
